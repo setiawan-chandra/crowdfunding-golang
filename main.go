f9f8216db9f5ac70a204828babc8e2be01a3b692
setiawan-chandra <wawantan17@gmail.com>
2024-08-29 07:50:02 +0700

    Feat: Strcuture File User & Test Get Data From Database

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crowdfunding-golang/user"
 	"fmt"
 	"log"
 
@@ -10,11 +11,25 @@ import (
 
 func main() {
 	dsn := "root:@tcp(127.0.0.1:3306)/crowdfunding-golang?charset=utf8mb4&parseTime=True&loc=Local"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
 	fmt.Println("Connection to database established")
+
+	var users []user.User
+	len_user := len(users)
+
+	db.Find(&users)
+
+	len_user = len(users)
+	fmt.Println(len_user)
+
+	for _, user := range users {
+		fmt.Println(user.Name)
+		fmt.Println(user.Email)
+		fmt.Println("=================")
+	}
 }
