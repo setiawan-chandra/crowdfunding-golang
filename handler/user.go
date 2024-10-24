724c0e768cde9c06295e210f59fbd59e48bdc0a5
setiawan-chandra <wawantan17@gmail.com>
2024-10-24 21:50:50 +0700

    Feat: JSON Response Helper

--- handler/user.go
+++ handler/user.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"crowdfunding-golang/helper"
 	"crowdfunding-golang/user"
 	"net/http"
 
@@ -28,5 +29,7 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, nil)
 	}
 
-	c.JSON(http.StatusOK, user)
+	response := helper.APIResponse("Account has been registered", http.StatusOK, "success", user)
+
+	c.JSON(http.StatusOK, response)
 }
