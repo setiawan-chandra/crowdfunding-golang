eed9d65614fa836aaaa56740a2776e498992788d
setiawan-chandra <wawantan17@gmail.com>
2024-10-30 22:27:59 +0700

    Feat: Repository User Login

--- main.go
+++ main.go
@@ -3,14 +3,13 @@ package main
 import (
 	"crowdfunding-golang/handler"
 	"crowdfunding-golang/user"
+	"fmt"
 	"log"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
-
-// "fmt"
 
 func main() {
 	dsn := "root:@tcp(127.0.0.1:3306)/crowdfunding-golang?charset=utf8mb4&parseTime=True&loc=Local"
@@ -22,6 +21,17 @@ func main() {
 
 	userRepository := user.NewRepository(db)
 	userService := user.NewService(userRepository)
+
+	userByEmail, err := userRepository.FindByEmail("email@gmail.com")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	if userByEmail.ID == 0 {
+		fmt.Println("User not found")
+	} else {
+		fmt.Println(userByEmail.Name)
+	}
 
 	userHandler := handler.NewUserHandler(userService)
 
