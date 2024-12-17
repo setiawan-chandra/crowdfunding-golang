377806a5bf4654ca12e378f47fa546b56bd741ab
setiawan-chandra <wawantan17@gmail.com>
2024-12-17 07:39:47 +0700

    Feat: Get CurrentUser

--- handler/user.go
+++ handler/user.go
@@ -137,7 +137,8 @@ func (h *userHandler) UploadAvatar(c *gin.Context) {
 		return
 	}
 
-	userID := 3
+	currentUser := c.MustGet("currentUser").(user.User)
+	userID := currentUser.ID
 
 	path := fmt.Sprintf("images/%d-%s", userID, file.Filename)
 
