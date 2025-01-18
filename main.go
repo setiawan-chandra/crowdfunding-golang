744d960c5225fabde56ef577b2c3ab41d9726095
setiawan-chandra <wawantan17@gmail.com>
2025-01-18 08:13:53 +0700

    Feat: Get List Campaings Service

--- main.go
+++ main.go
@@ -28,19 +28,12 @@ func main() {
 	userRepository := user.NewRepository(db)
 	campaignRepository := campaign.NewRepository(db)
 
-	campaigns, err := campaignRepository.FindByUserID(3)
-
-	fmt.Println(len(campaigns))
-	for _, campaign := range campaigns {
-		fmt.Println(campaign.Name)
-		if len(campaign.CampaignImages) > 0 {
-			fmt.Println("jumlah gambar")
-			fmt.Println(len(campaign.CampaignImages))
-			fmt.Println(campaign.CampaignImages[0].FileName)
-		}
-	}
-
 	userService := user.NewService(userRepository)
+	campaignService := campaign.NewService(campaignRepository)
+
+	campaign, _ := campaignService.FindCampaigns(100)
+	fmt.Println(len(campaign))
+
 	authService := auth.NewService()
 
 	userHandler := handler.NewUserHandler(userService, authService)
