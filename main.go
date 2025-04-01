5a10a1d7abf5fbc274b30bdcf95375e761e51682
setiawan-chandra <wawantan17@gmail.com>
2025-04-01 20:27:16 +0700

    Feat: Create New Transaction

--- main.go
+++ main.go
@@ -34,6 +34,14 @@ func main() {
 	authService := auth.NewService()
 	transactionService := transaction.NewService(transactionRepository, campaignRepository)
 
+	// user, _ := userService.GetUserByID(1)
+	// input := transaction.CreateTransactionInput{
+	// 	CampaignID: 1,
+	// 	Amount:     100000,
+	// 	User:       user,
+	// }
+	// transactionService.CreateTransaction(input)
+
 	userHandler := handler.NewUserHandler(userService, authService)
 	campaignHandler := handler.NewCampaignHandler(campaignService)
 	transactionHandler := handler.NewTransactionHandler(transactionService)
@@ -56,6 +64,7 @@ func main() {
 
 	api.GET("/campaigns/:id/transactions", authMiddleware(authService, userService), transactionHandler.GetCampaignTransactions)
 	api.GET("/transactions", authMiddleware(authService, userService), transactionHandler.GetUserTransactions)
+	api.POST("/transactions", authMiddleware(authService, userService), transactionHandler.CreateTransaction)
 
 	router.Run()
 }
--- transaction/repository.go
+++ transaction/repository.go
@@ -9,6 +9,7 @@ type repository struct {
 type Repository interface {
 	GetByCampaignID(campaignID int) ([]Transaction, error)
 	GetByUserID(UserID int) ([]Transaction, error)
+	Save(transaction Transaction) (Transaction, error)
 }
 
 func NewRepository(db *gorm.DB) *repository {
@@ -35,4 +36,14 @@ func (r *repository) GetByUserID(UserID int) ([]Transaction, error) {
 	}
 
 	return transactions, nil
+}
+
+func (r *repository) Save(transaction Transaction) (Transaction, error) {
+	err := r.db.Create(&transaction).Error
+
+	if err != nil {
+		return transaction, err
+	}
+
+	return transaction, nil
 }
